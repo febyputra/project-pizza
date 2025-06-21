e8afad75f79d0afbba0b1168c5efce677baa7f07
febyputra <febidianpramana@gmail.com>
2025-06-22 00:20:09 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const repoName = 'project-pizza'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/project-pizza/',
   plugins: [vue(), vueDevTools()],
+  base: `/${repoName}/`,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
