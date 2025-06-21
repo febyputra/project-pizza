e69e603637cc2db7fb599d0cbe182b63b88fcae2
febyputra <febidianpramana@gmail.com>
2025-06-21 17:02:27 +0700

    Update terbaru

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'project-pizza/',
+  base: '/project-pizza/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
